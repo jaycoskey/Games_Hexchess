a15e17ce0213f74aa1b2c61ee7cadfba433a160b
Jay Coskey <jay.coskey@gmail.com>
2021-09-01 01:04:52 -0700

    Add Qt signals and slots methods; add pgn_string() method

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -14,45 +14,117 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <iostream>
+#include <memory>
 
 #include "game.h"
 #include "game_outcome.h"
 #include "util_hexchess.h"
 
+using std::cout;
 using std::string;
 
 
 namespace hexchess::core {
 
-template<>
-std::shared_ptr<Player> Game<Glinski>::getPlayer(Color c) {
-    return c == Color::Black ? _bPlayer : _wPlayer;
+
+// TODO: Add two shared_ptr<Player> args and an optional FEN string to Game::Game()
+Game::Game(bool doPopulate)
+    : outcome{Termination::None},
+      _event{"Game played on Hexchess"},
+      _site{"Virtual"},
+      _round{"1"},
+      _variant{"Glinski"}
+{
+    player1 = std::make_shared<PlayerRandom>();
+    player2 = std::make_shared<PlayerRandom>();
+    board = Board<Glinski>{doPopulate};
+
+    // Set date and time strings
+    time_t unixTime = time(&unixTime);
+    struct tm* timeInfo = gmtime(&unixTime);
+    char buffer[100];
+
+    // Note: Could instead use std::put_time (since C++11)
+    strftime(buffer, sizeof(buffer), "%Y.%m.%d", timeInfo);
+    string date_{buffer, buffer + 10};
+    cout << "Current date = <<" << date_ << ">>\n";
+
+    strftime(buffer, sizeof(buffer), "%H:%M:%S", timeInfo);
+    string time_{buffer, buffer + 8};
+    cout << "Current time (UTC) = <<" << time_ << ">>\n";
 }
 
-template<>
-const GameOutcome& Game<Glinski>::play() {
-    throw NotImplementedException{"Board<Glinski>::play"};
+std::shared_ptr<Player> Game::getPlayer(Color c) {
+    return c == Color::Black ? player2 : player1;
+}
+
+void Game::setPlayer1(std::shared_ptr<Player>& p1) { player1 = p1; }
+void Game::setPlayer2(std::shared_ptr<Player>& p2) { player2 = p2; }
+
+const std::string Game::playerName(Color c) const {
+    return c == Color::Black ? player2->name() : player1->name();
+}
+
+void Game::receiveActionFromPlayer(Color mover, PlayerAction& action) {
+    switch(action.playerActionEnum()) {
+    case PlayerActionEnum::Move:
+        for (Move& legalMove : board.getLegalMoves(mover)) {
+            if (legalMove == action.move()) {
+                board.moveExec(legalMove);
+
+                if (board.getOptOutcome() != std::nullopt) {
+                    GameOutcome outcome = board.getOptOutcome().value();
+                    sendGameOutcomeToPlayer1(Color::White, outcome);
+                    sendGameOutcomeToPlayer2(Color::Black, outcome);
+                    // TODO: Handle end of game
+                }
+                if (board.isCheck()) {
+                    sendCheckToPlayers(mover, board.getKingIndex(mover));
+                }
+                Color nextMover = opponent(mover);
+                if (nextMover == Color::Black) {
+                    sendActionToPlayer2(nextMover, action);
+                } else {
+                    sendActionToPlayer1(nextMover, action);
+                }
+            }
+        }
+        break;
+    case PlayerActionEnum::Offer_Draw:
+        // TODO: Implement receivePlayerActionFromPlayer: Offer_Draw
+        break;
+    case PlayerActionEnum::Offer_Draw_Accept:
+        // TODO: Implement receivePlayerActionFromPlayer: Offer_Draw_Accept
+        break;
+    case PlayerActionEnum::Offer_Draw_Decline:
+        // TODO: Implement receivePlayerActionFromPlayer: Offer_Draw_Decline
+        break;
+    case PlayerActionEnum::Resign:
+        // TODO: Implement receivePlayerActionFromPlayer: Resign
+        break;
+    default:
+        throw std::logic_error("receivePlayerActionFromPlayer: Unrecognized player action");
+    }
 }
 
 /// \todo Add Game::game_outcome_json_string()
-template<>
-const string Game<Glinski>::game_summary_string() const {
+const string Game::game_summary_string() const {
     std::ostringstream oss;
 
     oss << "Players=("
-        <<   "White:\"" <<  _wPlayer->name() << "\", "
-        <<   "Black:\"" <<  _bPlayer->name() << "\", "
+        <<   "White:\"" <<  player1->name() << "\", "
+        <<   "Black:\"" <<  player2->name() << "\", "
         <<   "), "
         << "Outcome="
         ;
 
     if (outcome.isWin()) {
-        oss << "Win for " << color_string(outcome.winner)
-            << " by " << termination_string(outcome.termination);
-    } else if (outcome.termination == Termination::Draw_Stalemate) {
-        oss << "Stalemate by " << outcome.winner;
+        oss << "Win for " << color_string(outcome.winner())
+            << " by " << termination_string(outcome.termination());
+    } else if (outcome.termination() == Termination::Draw_Stalemate) {
+        oss << "Stalemate by " << outcome.winner();
     } else /* Other form of Draw */ {
-        oss << "Draw (" << termination_string(outcome.termination) << ")";
+        oss << "Draw (" << termination_string(outcome.termination()) << ")";
     }
     oss << ", ";
     oss << "Score(W)=" << outcome.score(Color::White) << ", ";
@@ -61,9 +133,24 @@ const string Game<Glinski>::game_summary_string() const {
     return oss.str();
 }
 
-template<>
-const string Game<Glinski>::pgn_string() const {
-    throw NotImplementedException{"Board<Glinski>::pgn_string"};
+const string Game::pgn_string() const {
+    std::ostringstream oss;
+
+    oss << "[Event "   << _event << "]\n";
+    oss << "[Site "    << _site << "]\n";
+    oss << "[Round "   << _round << "]\n";
+
+    oss << "[Variant " << _variant << "]\n";
+    oss << "[Date "    << _date << "]\n";
+    // oss << "[Time "    << _time << "]\n";
+    oss << "[White "   << playerName(Color::White) << "]\n";
+    oss << "[Black "   << playerName(Color::Black) << "]\n";
+    if (outcome.termination() != Termination::None) {
+        oss << "[Result " << outcome.game_outcome_score_string() << "]\n";
+    }
+    oss << board.board_pgn_string();
+
+    return oss.str();
 }
 
 }  // namespace hexchess::core
--- src/core/game.h
+++ src/core/game.h
@@ -15,51 +15,80 @@
 
 #pragma once
 
+#include <ctime>
+#include <time.h>
+
 #include <iostream>
 #include <memory>
+
+#include <QObject>
 
 #include "board.h"
 #include "game_outcome.h"
 #include "player.h"
+#include "player_simple_random.h"
 #include "util_hexchess.h"
 
 
 namespace hexchess::core {
 
-template <typename Variant>
-class Game {
+using player::Player;
+using player::PlayerRandom;
+
+class Game : public QObject {
+    Q_OBJECT
+    typedef Glinski V;
+
 public:
-    typedef Variant V;
-
-    const std::string pgn_string() const;
-
-    Game(std::shared_ptr<Player> wp, std::shared_ptr<Player> bp)
-        : _bPlayer{bp}, _wPlayer{wp}
-    { }
+    Game(bool doPopulate=true);
 
     const GameOutcome& play();
 
     std::shared_ptr<Player> getPlayer(Color c);
-    // void setPlayer(Color c, std::shared_ptr<Player> player);
-    const std::string playerName(Color c) {
-        return c == Color::Black ? _bPlayer->name() : _wPlayer->name();
-    }
+    void setPlayer1(std::shared_ptr<Player>& p1);
+    void setPlayer2(std::shared_ptr<Player>& p2);
+    const std::string playerName(Color c) const;
 
     const std::string game_summary_string() const;
+    const std::string pgn_string() const;
 
     Board<V> board;
     GameOutcome outcome;
+    std::shared_ptr<Player> player1;
+    std::shared_ptr<Player> player2;
+
+public slots:
+    void receiveActionFromPlayer(Color mover, PlayerAction& action);
+
+signals:
+    // Boardcast to Players
+    void sendBoardInitializationToPlayers(const Fen<V>& fen);
+    void sendCheckToPlayers(Color checked, Index kingInd);
+    void sendGameOutcomeToPlayer1(Color reader, const GameOutcome& outcome);
+    void sendGameOutcomeToPlayer2(Color reader, const GameOutcome& outcome);
+
+    // Sent individually
+    void sendActionRequestToPlayer1(Color mover);
+    void sendActionRequestToPlayer2(Color mover);
+
+    void sendActionToPlayer1(Color mover, PlayerAction& action);
+    void sendActionToPlayer2(Color mover, PlayerAction& action);
 
 private:
-    std::shared_ptr<Player> _bPlayer;
-    std::shared_ptr<Player> _wPlayer;
-
     void _announceGameEnd(const GameOutcome &outcome) const;
     void _printGameStats() const;
 
     // ---------- Private write methods
-    void _initPlayers();
+    // void _initPlayers();
     void _reset();
+
+    std::string _event;
+    std::string _site;
+    std::string _round;
+    std::string _variant;
+
+    std::string _date;
+    std::string _time;
 };
 
 }  // namespace hexchess::core
