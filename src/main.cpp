181f0295fee357df4809092b4e4dcd814c212e7f
Jay Coskey <jay.coskey@gmail.com>
2021-09-01 22:57:52 -0700

    Add to main the handling of different Player types, including wiring up Qt signals/slots

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,62 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <QApplication>
+#include <cstring>
+
+#include <iostream>
+
 
 #include "board.h"
+#include "game.h"
+#include "player.h"
+#include "player_human_text.h"
+#include "player_simple_random.h"
 
 #include "ui/boardwidget.h"
-#include "ui/mainWindow.h"
+#include "ui/mainwindow.h"
 
+using std::cout;
 
+using hexchess::core::Color;
+using hexchess::core::Game;
+
+using hexchess::player::connectSignalsToSlots;
+using hexchess::player::Player;
+using hexchess::player::PlayerHumanText;
+using hexchess::player::PlayerRandom;
+
+/// \todo Modify to support multiple variants
 int main(int argc, char *argv[]) {
+    cout << "Entering main\n";
+
     QApplication a(argc, argv);
 
+    Game game{false};
     MainWindow mw{};
-    mw.show();
 
+    if (argc == 2 && strcmp(argv[1], "--text") == 0) {
+        std::shared_ptr<Player> player1 = std::make_shared<PlayerHumanText>();
+        std::shared_ptr<Player> player2 = std::make_shared<PlayerHumanText>();
+        game.setPlayer1(player1);
+        game.setPlayer2(player2);
+
+        PlayerHumanText *p1 = dynamic_cast<PlayerHumanText*>(player1.get());
+        PlayerHumanText *p2 = dynamic_cast<PlayerHumanText*>(player2.get());
+
+        hexchess::player::connectSignalsToSlots(game, p1, p2);
+        game.sendBoardInitializationToPlayers(Glinski::fenInitial);
+        game.sendActionRequestToPlayer1(Color::White);
+    } else {
+        std::shared_ptr<Player> player1 = std::make_shared<PlayerRandom>();
+        std::shared_ptr<Player> player2 = std::make_shared<PlayerRandom>();
+        game.setPlayer1(player1);
+        game.setPlayer2(player2);
+
+        PlayerRandom *p1 = dynamic_cast<PlayerRandom*>(player1.get());
+        PlayerRandom *p2 = dynamic_cast<PlayerRandom*>(player2.get());
+
+        hexchess::player::connectSignalsToSlots(game, p1, p2);
+        mw.show();
+    }
     return a.exec();
 }
