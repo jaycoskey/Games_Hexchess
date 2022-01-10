8a76238f31a2675e3cd2e855fe4e0dbe44114dff
Jay M. Coskey <jay.coskey@gmail.com>
2022-01-10 00:02:37 -0800

    Standardize names. Add cache priming and debug printing. Add stub for receiveActionFromGui.

--- src/player/player_alpha_beta.cpp
+++ src/player/player_alpha_beta.cpp
@@ -29,38 +29,136 @@ using core::Board;
 using core::Move;
 using core::PlayerAction;
 using core::PlayerActionEnum;
+using core::Scope;
 
 // ========================================
+// Game <--> Player
+
+// --------------------
 // Broadcast
-void PlayerAlphaBeta::initializeBoard(const Fen<V>& fen) {
+void PlayerAlphaBeta::receiveBoardInitializationFromServer(const Fen<Glinski>& fen) {
+    Scope scope{"PlayerAlphaBeta::receiveBoardInitializationFromServer"};
+
+    if (hexchess::events_verbose) {
+        print(cout, scope(), "Player ", _name,
+            " (counter=", _board.currentCounter(), ")",
+            " receives board initialization\n");
+    }
     _board.initialize(fen);
-}
-
-void PlayerAlphaBeta::showCheckToPlayer(Color checked, Index kingInd) {
-    // No action needed; This simple computer player doesn't handle check.
-}
-
-// ========================================
-// Sent individually
-void PlayerAlphaBeta::showActionRequestToPlayer(Color mover) {
-    const Moves& moves = _board.getLegalMoves(mover);
-    assert(moves.size() > 0);
-    const auto& [optBestMove, _] = searchAlphaBeta(_board, mover, _minDepth);
-    PlayerAction action{optBestMove.value()};
-    sendActionToServer(mover, action);
-}
-
-void PlayerAlphaBeta::showActionToPlayer(Color mover, PlayerAction& action) {
-    // TODO: Support Draw offers, acceptances, and declines
-    if (action.playerActionEnum() == PlayerActionEnum::Move) {
-        _board.moveExec(action.move());
+    if (_gui) {
+        if (hexchess::events_verbose) {
+            print(cout, scope(), "Player ", _name,
+                " (counter=", _board.currentCounter(), ")",
+                " sending board initialization to GUI\n");
+        }
+        sendBoardInitializationToGui(fen);
     }
 }
 
-void PlayerAlphaBeta::showGameOutcomeToPlayer(Color reader,
-                                              const GameOutcome& gameOutcome)
+void PlayerAlphaBeta::receiveCheckFromServer(Color checked, Index kingInd) {
+    Scope scope{"PlayerAlphaBeta::receiveCheckFromServer"};
+
+    // No local action needed; Computer player handles checks.
+    sendCheckToGui(checked, kingInd);
+}
+
+// --------------------
+// Sent individually
+
+void PlayerAlphaBeta::receiveActionRequestFromServer(Color mover, const Moves legalMoves) {
+    Scope scope{"PlayerAlphaBeta::receiveActionRequestFromServer"};
+
+    if (hexchess::events_verbose) {
+        cout << "============================================================\n";
+        print(cout, scope(), "Player ", _name, " (mover=", color_long_string(mover),
+            ", counter=", _board.currentCounter(), ") ",
+            "receives action request #", _board.currentCounter() + 1,
+            " (", _board.pieceCount(), " pieces on board)....\n");
+    }
+    const Moves& moves = _board.getLegalMoves(mover);
+    assert(moves.size() > 0);
+    auto [optBestMove, _] = searchAlphaBeta(_board, mover, _minDepth);
+    assert(optBestMove.has_value());
+    _board.moveExec(optBestMove.value());
+    (void) _board.getLegalMoves(_board.mover());
+    (void) _board.getCheckEnum();
+
+    PlayerAction action{optBestMove.value()};
+    // No need to ask human for move
+    // if (_gui) {
+    //     sendActionRequestToGui(mover, legalMoves);
+    // }
+    if (hexchess::events_verbose) {
+        print(cout, scope(), "Player ", _name, " (", color_long_string(mover),
+            ", counter=", _board.currentCounter(), ")",
+            ", sending action by ", color_long_string(action.move().mover()),
+            " to server: ", action.player_action_string(false), "\n");
+    }
+    sendActionToServer(mover, action);
+
+    if (_gui) {
+        if (hexchess::events_verbose) {
+            print(cout, scope(), "Player ", _name, " (", color_long_string(mover),
+                ", counter=", _board.currentCounter(), ")",
+                ", sending action by ", color_long_string(action.move().mover()),
+                " to GUI: ", action.player_action_string(false), "\n");
+        }
+        sendActionToGui(mover, action);
+    }
+}
+
+void PlayerAlphaBeta::receiveActionFromServer(Color mover, const PlayerAction action) {
+    Scope scope{"PlayerAlphaBeta::receiveActionFromServer"};
+
+    if (hexchess::events_verbose) {
+        print(cout, scope(), "Player ", _name, " (", mover,
+            ", counter=", _board.currentCounter(), ") ",
+            "receives opponent's action: ", action.player_action_string(false), "\n");
+    }
+    // TODO: Support Draw offers, acceptances, and declines
+    if (action.playerActionEnum() == PlayerActionEnum::Move) {
+        if (hexchess::events_verbose) {
+            print(cout, scope(), "Player ", _name, " (", mover,
+                ", counter=", _board.currentCounter(), ") ",
+                 "carrying out opponent move: ",
+                 action.move().move_pgn_string(false), "\n");
+        }
+        _board.moveExec(action.move());
+        (void) _board.getLegalMoves(_board.mover());
+        (void) _board.getCheckEnum();
+    }
+    if (_gui) {
+        if (hexchess::events_verbose) {
+            print(cout, scope(), "Player ", _name, " (mover=", color_long_string(mover),
+                ", counter=", _board.currentCounter(), ") ",
+                " sending action to GUI\n");
+        }
+        sendActionToGui(mover, action);
+    }
+}
+
+void PlayerAlphaBeta::receiveGameOutcomeFromServer(
+    Color receiver, const GameOutcome& gameOutcome)
 {
+    Scope scope{"PlayerAlphaBeta::receiveGameOutcomeFromServer"};
+
     // No action needed for this computer player
+    if (_gui) {
+        if (hexchess::events_verbose) {
+            print(cout, scope(), "Player ", _name, " (received by ", color_long_string(receiver),
+                ", counter=", _board.currentCounter(), ") ",
+                " sending game outcome to GUI\n");
+        }
+        sendGameOutcomeToGui(receiver, gameOutcome);
+    }
+}
+
+// ========================================
+// Player <--> GUI
+
+void PlayerAlphaBeta::receiveActionFromGui(Color mover, const PlayerAction& action) {
+    // This is a computer player; it should not receive actions from the GUI.
+    throw std::logic_error{"PlayerAlphaBeta, a computer player, should not receive player actions from the GUI"};
 }
 
 }  // namespace hexchess::player
